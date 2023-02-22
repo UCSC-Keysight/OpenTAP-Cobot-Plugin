0f70f5560c24c27a7637f64c6c697b48f2feffe2
Jonah <jonahlavi@yahoo.com>
2023-02-22 11:43:09 -0800

    succesfully connected to Cobot with Modbus TCP comms.

--- UR3e.py
+++ UR3e.py
@@ -1,6 +1,8 @@
 from opentap import *
 from System import Double, String
 import OpenTap
+import time
+from pymodbus.client import ModbusTcpClient
 import socket
 
 @attribute(OpenTap.Display("UR3e", "UR3e driver.", "UR_Prototype"))
@@ -18,50 +20,49 @@ class UR3e(Instrument):
     #                 response.
     @method(Double)
     def send_request_movement(self, FilePath):
-
-        HOST = self.IpAddress
-        PORT = 30002
-
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-            client_socket.settimeout(4)
-
-            self.log.Info("Connecting with {}:{}".format(self.IpAddress, PORT))
-            try:
-                client_socket.connect((HOST, PORT))
-            except socket.timeout as e:
-                self.log.Error("Timeout error: {}".format(e))
-                return False
-            except socket.error as e:
-                self.log.Error("Could not connect to {}:{} Error: {}".format(HOST, PORT, e))
-                return False
-            try:
-                self.log.Debug(FilePath)
-                #Open and parse file
-                f = open(FilePath, "r")
-                allCommands = f.readlines()
-                for curCommand in allCommands:
-                    curCommand + '\n'
-                    self.log.Info(f"Sending command {curCommand!r}")
-                    client_socket.sendall(curCommand.encode())
-                    response = client_socket.recv(1024)    
-                    self.log.Info(f"Client received: {response!r}")      
-
-            except socket.error as e:
-                self.log.Error("Sendall failed. Error: {}".format(e))
-                return False
-
-        if response:
-            # Not sure how to deserialize response.
-            # Nothing in documentation about its encoding.
-            # https://forum.universal-robots.com/t/how-do-i-deserialize-response-messages-from-the-controller/26537
-
-            self.log.Warning("This response is serialized.")
-            client_socket.close()
-            return True
-        else:
-            self.log.Error("No response message received.")
-            client_socket.close()
+         # Connect to the MODBUS server on the cobot
+        client = ModbusTcpClient(self.IpAddress)
+        try:
+            client.connect()
+            #print("Connection Success")
+        except Exception as e:
+            self.log.Error("Could not connect to cobot. Error: {}".format(e))
+            #print("Connection Failed")
             return False
+        try:
+            # Open and parse file
+            f = open(FilePath, "r")
+            allCommands = f.readlines()
+            for curCommand in allCommands:
+                curCommand + '\n'
+                self.log.Info(f"Sending command {curCommand!r}")
+                # Send the command to the cobot using Modbus function code 6 (write single register)
+                register_address = 0
+                register_value = curCommand.encode()
+                result = client.write_register(address=register_address, value=register_value, unit=0)
+                if result.isError():
+                    #self.log.Error("Error writing register: {}".format(result))
+                    print(curCommand + " command not received")
+                    return False
+                else:
+                    print(curCommand + " received command ")
+                # Wait for the cobot to process the command
+                time.sleep(0.1)
+                # Read the response from the cobot using Modbus function code 3 (read holding registers)
+                register_address = 1
+                register_count = 1
+                result = client.read_holding_registers(address=register_address, count=register_count, unit=0)
+                if result.isError():
+                    self.log.Error("Error reading register: {}".format(result))
+                    return False
+                response = result.registers[0].to_bytes(2, byteorder='big').decode('utf-8')
+                self.log.Info(f"Client received: {response!r}")
+        except Exception as e:
+            self.log.Error("Error sending command. Error: {}".format(e))
+            return False
+
+        client.close()
+        return True
 
     def Open(self):
         """Called by TAP when the test plan starts."""
