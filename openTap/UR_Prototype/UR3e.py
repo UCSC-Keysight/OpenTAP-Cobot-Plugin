2815a667ff4f729d8b9a5bf1f404705cd03fcc70
Shayan-Bathaee <shayanbathaee@gmail.com>
2023-04-20 14:22:50 -0700

    move wait added to send request from file function in UR3e.py

--- openTap/UR_Prototype/UR3e.py
+++ openTap/UR_Prototype/UR3e.py
@@ -39,7 +39,6 @@ class UR3e(Instrument):
                 self.log.Error("Could not connect to {}:{} Error: {}".format(HOST, PORT, e))
                 return False
             try:
-                command = command + '\n'
                 # get target joint positions
                 target_pos_list = command.split("[")[1]
                 target_pos_list = target_pos_list.split("]")[0]
@@ -47,6 +46,7 @@ class UR3e(Instrument):
                 target_pos_list = [round(float(i), (5)) for i in target_pos_list]
                 
                 # send commnad
+                command = command + '\n'
                 self.log.Info(f"Sending command {command!r}")
                 client_socket.sendall(command.encode())
 
@@ -70,10 +70,9 @@ class UR3e(Instrument):
                             self.log.Info("Target position " + str(target_pos_list) + " reached")
                             break
 
-
                     if (time.time() - start) > 10: # allow 10 seconds before timeout
                         self.log.Info("Request timed out")
-                        break        
+                        break
                 
             except socket.error as e:
                 self.log.Error("Send command failed. Error: {}".format(e))
@@ -110,22 +109,47 @@ class UR3e(Instrument):
                 f = open(file_path, "r")
                 all_commands = f.readlines()
                 for cur_command in all_commands:
+                    # get target joint positions
+                    target_pos_list = cur_command.split("[")[1]
+                    target_pos_list = target_pos_list.split("]")[0]
+                    target_pos_list = target_pos_list.split(",")
+                    target_pos_list = [round(float(i), (5)) for i in target_pos_list]
+
+                    # send command
                     if (cur_command[-1] != '\n'):
                         cur_command = cur_command + '\n'
                     self.log.Info(f"Sending command {cur_command!r}")
                     client_socket.sendall(cur_command.encode())
-                    response = client_socket.recv(1024)
-                    self.log.Info(f"Client received: {response!r}")      
+
+                    # read joint positions until they equal target joint positions
+                    start = time.time()
+                    while True:
+                        new_message = client_socket.recv(4096)
+                        new_package = Package(new_message)
+                        subpackage = new_package.get_subpackage("Joint Data")
+                        if subpackage is not None:
+                            actual_pos_list = [
+                                subpackage.subpackage_variables.Joint1_q_actual,
+                                subpackage.subpackage_variables.Joint2_q_actual,
+                                subpackage.subpackage_variables.Joint3_q_actual,
+                                subpackage.subpackage_variables.Joint4_q_actual,
+                                subpackage.subpackage_variables.Joint5_q_actual,
+                                subpackage.subpackage_variables.Joint6_q_actual
+                            ]
+                            actual_pos_list = [round(i, 5) for i in actual_pos_list]
+                            if actual_pos_list == target_pos_list:
+                                self.log.Info("Target position " + str(target_pos_list) + " reached")
+                                break
+
+                        if (time.time() - start) > 10: # allow 10 seconds before timeout
+                            self.log.Info("Request timed out")
+                            break
 
             except socket.error as e:
                 self.log.Error("Send command failed. Error: {}".format(e))
                 return False
 
-        if response:
-            # Not sure how to deserialize response.
-            # Nothing in documentation about its encoding.
-            # https://forum.universal-robots.com/t/how-do-i-deserialize-response-messages-from-the-controller/26537
-            self.log.Warning("This response is serialized.")
+        if new_message:
             client_socket.close()
             return True
         else:
