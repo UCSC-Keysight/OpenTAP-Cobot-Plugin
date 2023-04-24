1d97df92729e49a93e4ce8e1dc1f986ec1936609
Shayan-Bathaee <shayanbathaee@gmail.com>
2023-04-24 13:25:26 -0700

    added GetJointState test step

--- openTap/UR_Prototype/UR3e.py
+++ openTap/UR_Prototype/UR3e.py
@@ -2,6 +2,9 @@ from opentap import *
 from System import Double, String
 import OpenTap
 import socket
+import time
+from .package import *
+
 
 @attribute(OpenTap.Display("UR3e", "UR3e driver.", "UR_Prototype"))
 class UR3e(Instrument):
@@ -90,7 +93,7 @@ class UR3e(Instrument):
             except socket.error as e:
                 self.log.Error("Send command failed. Error: {}".format(e))
                 return False
-
+            
         if response:
             # Not sure how to deserialize response.
             # Nothing in documentation about its encoding.
@@ -102,7 +105,49 @@ class UR3e(Instrument):
             self.log.Error("No response message received.")
             client_socket.close()
             return False
+        
 
+    # DESCRIPTION:    Open socket, connect to cobot, read message stream from cobot
+    #                 until it recieves joint state data or 3 seconds have passed.
+    # RETURNS:        List of joint state data if recieved, False if no joint data was
+    #                 recieved after 3 seconds. 
+    def get_joint_state(self):
+        HOST = self.ip_address
+        PORT = 30002
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            client_socket.settimeout(4)
+
+            self.log.Info("Connecting with {}:{}".format(self.ip_address, PORT))
+            try:
+                client_socket.connect((HOST, PORT))
+            except socket.timeout as e:
+                self.log.Error("Timeout error: {}".format(e))
+                return False
+            except socket.error as e:
+                self.log.Error("Could not connect to {}:{} Error: {}".format(HOST, PORT, e))
+                return False
+            
+            # read messages from UR3e for 3 seconds or until we recieve joint state
+            start = time.time()
+            while (time.time() - start) < 3:
+                new_message = client_socket.recv(2096)
+                new_package = Package(new_message)
+                subpackage = new_package.get_subpackage("Joint Data")
+                if subpackage is not None:
+                    actual_pos_list = [
+                        subpackage.subpackage_variables.Joint1_q_actual,
+                        subpackage.subpackage_variables.Joint2_q_actual,
+                        subpackage.subpackage_variables.Joint3_q_actual,
+                        subpackage.subpackage_variables.Joint4_q_actual,
+                        subpackage.subpackage_variables.Joint5_q_actual,
+                        subpackage.subpackage_variables.Joint6_q_actual
+                    ]
+                    actual_pos_list = [round(i, 5) for i in actual_pos_list]
+                    return actual_pos_list
+                
+            return False
+            
 
     def Open(self):
         """Called by TAP when the test plan starts."""
